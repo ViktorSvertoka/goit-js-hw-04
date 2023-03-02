418bbe9ca52462b126fb5a89412f6fbe4994278c
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-03-02 02:10:15 +0200

    Docs(text) added task description

---
+++ js/script-2.js
@@ -0,0 +1,36 @@
+// # Завдання 2
+
+// Виправ помилки, які будуть в консолі, щоб скрипт запрацював.
+
+// ```js
+// const inventory = {
+//   items: ["Knife", "Gas mask"],
+//   add(itemName) {
+//     console.log(`Adding ${itemName} to inventory`);
+
+//     this.items.push(itemName);
+//   },
+//   remove(itemName) {
+//     console.log(`Removing ${itemName} from inventory`);
+
+//     this.items = this.items.filter((item) => item !== itemName);
+//   },
+// };
+
+// const invokeInventoryAction = function (itemName, action) {
+//   console.log(`Invoking action on ${itemName}`);
+//   action(itemName);
+// };
+
+// invokeInventoryAction("Medkit", inventory.add);
+// // Invoking action on Medkit
+// // Adding Medkit to inventory
+
+// console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+// invokeInventoryAction("Gas mask", inventory.remove);
+// // Invoking action on Gas mask
+// // Removing Gas mask from inventory
+
+// console.log(inventory.items); // ['Knife', 'Medkit']
+// ```
