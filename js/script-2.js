66c950ca4f77bbfdc0d355acdb983a12b540bc66
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-03-02 22:08:24 +0200

    Update script-2.js

--- js/script-2.js
+++ js/script-2.js
@@ -18,13 +18,9 @@ const invokeInventoryAction = function (itemName, action) {
 };
 
 invokeInventoryAction('Medkit', inventory.add.bind(inventory));
-// Invoking action on Medkit
-// Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
 invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
-// Invoking action on Gas mask
-// Removing Gas mask from inventory
 
 console.log(inventory.items); // ['Knife', 'Medkit']
